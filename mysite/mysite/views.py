22fb79bdcab701b5d482c1de73d885fa3384500b
CHESTERFIELD <Michenko.nik@gmail.com>
2020-05-18 01:36:36 +0300

    add items

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from rozetka.models import Product
+
 
 def home(request):
-    return render(request, 'base.html')
+    queryset = Product.objects.order_by("-sale")[:32]
+    return render(request, 'base.html', context={"objects": queryset})
+
+def most_sale(request):
+    # queryset = Product.objetcs.max(sale)[:32]
+    return render(request, )
